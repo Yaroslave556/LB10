f39acb9cf0f86e88659e8f8edbf0bd9f79170b13
Yaroslave556 <113922320+Yaroslave556@users.noreply.github.com>
2024-03-26 22:17:59 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,32 @@
+import picture from "./dg (2).png"
+import "./style.css"
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container1 cont" id="cont1">
+        <header>
+          <div class="logo">
+            <p> project <span>REACT</span></p>
+          </div>
+          <nav>
+            <ul>
+              <li><a href="#cont1">Головна</a></li>
+              <li><a href="#cont2">Наші розробки</a></li>
+              <li><a href="#cont3">Пропозиції</a></li>
+              <li><a href="#cont4">Контакти</a></li>
+            </ul>
+          </nav>
+        </header>
+        <main>
+          <img id="back" src={picture} alt="dthsrt" />
+        </main>
+        <input></input>
+      </div>
+      <section></section>
+      <footer></footer>
+    </div>
+  );
+}
+
+export default App;
